1c7f2c7228332693c88605c53f2b9c399cac1e82
Tolu Alimi <talimi2000@gmail.com>
2024-01-26 03:12:23 -0600

    Fixed path to react app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import path from "path";
 import express from "express";
 import apiRoutes from "./api/index.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const router = express.Router();
 
@@ -8,6 +10,8 @@ const router = express.Router();
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 router.use(function(_req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
 });
