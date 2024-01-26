6a7692c5fd7a2378db789cb6f672dc0776d923df
Tolu Alimi <talimi2000@gmail.com>
2024-01-26 02:56:55 -0600

    Defined middleware, added routes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
-import express, { json } from 'express';
+import express, { json, urlencoded, static as static_ } from 'express';
 // import db from '../config/db.js';
 import cors from 'cors';
 import dotenv from "dotenv";
+import router from '../routes/index.js';
 
 const app = express();
 const corsOptions = {
     origin: "http://localhost:8081"
 };
+dotenv.config();
 
+// Define middleware
 app.use(cors(corsOptions));
 app.use(json());
+app.use(urlencoded({ extended: true }));
 
-dotenv.config();
-const PORT = process.env.PORT || 3002;
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(static_("client/build"));
+}
 
-app.get("/", (_req, res) => {
-    res.json("Welcome to the ChinDash application.");
-});
+// Add view and API routes
+app.use(router);
+
 
 // app.get("/api/genres", (_req, res) => {
 //     db.query(
@@ -40,6 +46,8 @@ app.get("/", (_req, res) => {
 //         }
 //     );
 // });
+
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
