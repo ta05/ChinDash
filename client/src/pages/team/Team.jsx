464358c49335ae2d29ae5ebfb347504d77cfeaae
Tolu Alimi <talimi2000@gmail.com>
2024-01-27 06:12:52 -0600

    Added genre query to teams page

--- client/src/pages/team/Team.jsx
+++ client/src/pages/team/Team.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
 
 const Team = () => {
-    return <div>Team</div>;
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        loadGenres();
+    }, []);
+
+    const loadGenres = () => {
+        API.getGenres()
+            .then((res) => {
+                setGenres(res.data);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    return (
+        <div>
+            {genres.map((genre, index) => (
+                <h2 key={index}>{genre["Name"]}</h2>
+            ))}
+        </div>
+    );
 };
 
 export default Team;
