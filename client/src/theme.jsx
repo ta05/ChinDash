f5c2825c4ee0cf92abfdd2e04bb793770116a0f3
Tolu Alimi <talimi2000@gmail.com>
2024-01-30 00:24:30 -0600

    Corrected curly brace which caused the typography settings not to be incorporated into MUI components

--- client/src/theme.jsx
+++ client/src/theme.jsx
@@ -128,7 +128,7 @@ export const themeSettings = (mode) => {
 
     return {
         palette: {
-            mode,
+            mode: mode,
             ...(mode == "dark"
                 ? {
                       primary: {
@@ -162,33 +162,33 @@ export const themeSettings = (mode) => {
                           default: "#fcfcfc",
                       },
                   }),
-            typography: {
+        },
+        typography: {
+            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
                 fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 12,
-                h1: {
-                    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                    fontSize: 40,
-                },
-                h2: {
-                    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                    fontSize: 32,
-                },
-                h3: {
-                    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                    fontSize: 24,
-                },
-                h4: {
-                    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                    fontSize: 20,
-                },
-                h5: {
-                    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                    fontSize: 16,
-                },
-                h6: {
-                    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                    fontSize: 14,
-                },
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 14,
             },
         },
     };
