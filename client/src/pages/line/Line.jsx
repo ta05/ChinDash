6e39c2fd092da7bccb6ed8be2620bfee4f7e663b
Tolu Alimi <talimi2000@gmail.com>
2024-01-31 22:55:34 -0600

    Added LineChart from nivo

--- client/src/pages/line/Line.jsx
+++ client/src/pages/line/Line.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import LineChart from "../../components/LineChart";
+import API from "../../utils/API";
 
 const Line = () => {
-    return <div>Line</div>;
+    const [genreSales, setGenreSales] = useState([]);
+
+    const loadGenreSales = (signal) => {
+        API.getGenreSales(signal)
+            .then((res) => {
+                setGenreSales(res.data);
+            })
+            .catch((err) => {
+                if (err === "AbortError") {
+                    console.log("cancelled!");
+                } else {
+                    console.log(err);
+                }
+            });
+    };
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        loadGenreSales(signal);
+
+        return () => {
+            controller.abort();
+        };
+    }, []);
+
+    return (
+        <div>
+            <LineChart data={genreSales} />
+        </div>
+    );
 };
 
 export default Line;
